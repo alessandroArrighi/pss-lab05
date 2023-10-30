66ea516b2160bcfb894caa2c14b3b9f10ba76be2
alessandroArrighi <alessandro.arrighi2@studio.unibo.it>
2023-10-30 20:33:41 +0100

    create Exam class with fields

---
+++ 03-composition/src/main/java/it/unibo/composition/Exam.java
@@ -0,0 +1,11 @@
+package it.unibo.composition;
+
+public class Exam {
+    private int id;
+    private int maxStudents;
+    private int registeredStudents;
+    private String courseName;
+    private Professor professor;
+    private ExamRoom room;
+    private Student[] students;
+}
