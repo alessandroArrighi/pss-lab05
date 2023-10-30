27438bfe0b48a48f7542dec32ac7cae5b086acc5
alessandroArrighi <alessandro.arrighi2@studio.unibo.it>
2023-10-30 19:59:40 +0100

    create costructor

--- 03-composition/src/main/java/it/unibo/composition/Professor.java
+++ 03-composition/src/main/java/it/unibo/composition/Professor.java
@@ -1,9 +1,16 @@
 package it.unibo.composition;
 
 public class Professor implements User {
-    int id;
-    String name;
-    String username;
-    String password;
-    String[] courses;
+    public int id;
+    public String name;
+    public String username;
+    public String password;
+    public String[] courses;
+
+    Professor(final int id, final String password) {
+        this.id = id;
+        this.password = password;
+    }
+
+    
 }
