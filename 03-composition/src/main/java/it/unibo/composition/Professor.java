a3643ea6f41cbdc7e52dc8d64af343ddfff24013
alessandroArrighi <alessandro.arrighi2@studio.unibo.it>
2023-10-30 20:06:19 +0100

    change wrong public fields into privete one

--- 03-composition/src/main/java/it/unibo/composition/Professor.java
+++ 03-composition/src/main/java/it/unibo/composition/Professor.java
@@ -1,16 +1,18 @@
 package it.unibo.composition;
 
 public class Professor implements User {
-    public int id;
-    public String name;
-    public String username;
-    public String password;
-    public String[] courses;
+    private int id;
+    private String name;
+    private String username;
+    private String password;
+    private String[] courses;
 
-    Professor(final int id, final String password) {
+    public Professor(final int id, final String password) {
         this.id = id;
         this.password = password;
     }
 
-    
+    public void replaceCourse(String course, int index) {
+        
+    }
 }
