745d36836689b08c20b3009c448d238464c327c0
alessandroArrighi <alessandro.arrighi2@studio.unibo.it>
2023-10-30 20:30:05 +0100

    create replaceAllCourses getUsername getPassword getDescription methods

--- 03-composition/src/main/java/it/unibo/composition/Professor.java
+++ 03-composition/src/main/java/it/unibo/composition/Professor.java
@@ -3,7 +3,7 @@ package it.unibo.composition;
 public class Professor implements User {
     private int id;
     private String name;
-    private String username;
+    private String surname;
     private String password;
     private String[] courses;
 
@@ -12,9 +12,35 @@ public class Professor implements User {
         this.password = password;
     }
 
+    @Override
+    public String toString() {
+        return "Professor ["
+        + "name=" + this.name
+        + ", surname=" + this.surname
+        + ", id=" + this.id
+        + ", password=" + this.password
+        + "]";
+    }
+
+    public String getUsername() {
+        return this.surname;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getDescription() {
+        return toString();
+    }
+
     public void replaceCourse(String course, int index) {
         if(this.courses.length > index) {
             this.courses[index] = course;
         }
+    }
+
+    public void replaceAllCourses(String[] courses) {
+        this.courses = courses;
     }
 }
