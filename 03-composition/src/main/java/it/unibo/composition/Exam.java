be82ae73e879128509582b2921a863e1c6e6a60a
alessandroArrighi <alessandro.arrighi2@studio.unibo.it>
2023-11-01 18:36:07 +0100

    create registerStudent method

--- 03-composition/src/main/java/it/unibo/composition/Exam.java
+++ 03-composition/src/main/java/it/unibo/composition/Exam.java
@@ -47,6 +47,12 @@ public class Exam {
         return this.students;
     }
 
+    public void registerStudent(Student student) {
+        if(this.students.length < this.maxStudents) {
+            this.students[this.registeredStudents] = student;
+        }
+    }
+
     private void changeNumRegistredStudent(final int registeredStudents) {
         this.registeredStudents = registeredStudents;
     }
